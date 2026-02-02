be039e8dfc16bb27b7e3e7493c2b911ba5508ec3
Ajit <ajit@Ajits-MacBook-Pro.local>
2026-02-02 22:50:52 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // --- Static Export Configuration ---
-  // Enables export to static HTML
-  output: 'export',
-  distDir: 'out',
-  // --- Trailing Slash ---
-  // Good for static hosting (avoids 404s)
-  trailingSlash: true,
-
-  // --- Image Optimization ---
-  images: {
-    unoptimized: true, // disables on-demand image optimization
-  },
-
-  // Optional: if you want a custom output folder for export
-  // The default for next export is 'out'
-  // You can specify like: "next export -o build"
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
